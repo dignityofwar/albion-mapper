560c295018c68e36f0c3880e99f3f63e4ec5519e
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2026-04-27 00:57:27 +0100

    fix: speculative fix for vercel build

--- web/client/vite.config.ts
+++ web/client/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'shared': fileURLToPath(new URL('../shared/src/index.ts', import.meta.url)),
     },
   },
   server: {
