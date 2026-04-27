ed618da2f5f0d9d9defb1a7344b8d00f9e659ce2
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2026-04-27 01:17:27 +0100

    debug

--- web/client/src/main.ts
+++ web/client/src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import { router } from './router/index.js';
+import { API_BASE_URL } from './utils/api';
 import './style.css';
+
+console.log('API_BASE_URL:', API_BASE_URL);
 
 const app = createApp(App);
 app.use(createPinia());
