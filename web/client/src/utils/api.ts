89dcc30f5713079a979ffd53d17e4e733e736663
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2026-04-27 01:14:44 +0100

    fix: Speculative fix for API base url env issue

--- web/client/src/utils/api.ts
+++ web/client/src/utils/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.API_BASE_URL ?? 'https://roadmap-api.dignityofwar.com';
+export const API_BASE_URL = import.meta.env.NEXT_PUBLIC_API_BASE_URL ?? 'localhost:5432';
