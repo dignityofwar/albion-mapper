965accbb9cad4e9ddf614cf65cb1a2cc8e0e50f4
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2026-04-30 14:36:18 +0100

    feat: Exclude analytics if not prod

--- web/client/src/App.vue
+++ web/client/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import { Analytics } from '@vercel/analytics/vue';
+
+const isProd = import.meta.env.PROD;
 </script>
 
 <template>
-  <Analytics />
+  <Analytics v-if="isProd" />
   <RouterView />
 </template>
