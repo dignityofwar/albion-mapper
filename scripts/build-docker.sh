07a645629c3dab9137dfc8f16675da383e752236
Matt Cavanagh <1776058+Maelstromeous@users.noreply.github.com>
2026-05-22 19:27:28 +0100

    fix: Check platform before build

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 cd "$(dirname "$0")/.."
-docker buildx build --platform linux/amd64 . -f provisioning/Dockerfile -t maelstromeous/applications:dig-roadmap-latest --push
-echo "Docker build completed"
+
+if [ "$1" == "test" ]; then
+  IMAGE_TAG="maelstromeous/applications:dig-roadmap-testing"
+  BUILD_MSG="Docker test build completed"
+else
+  IMAGE_TAG="maelstromeous/applications:dig-roadmap-latest"
+  BUILD_MSG="Docker build completed"
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  docker buildx build --platform linux/amd64 . -f provisioning/Dockerfile -t $IMAGE_TAG --push
+else
+  docker build --platform linux/amd64 . -f provisioning/Dockerfile -t $IMAGE_TAG
+  docker push $IMAGE_TAG
+fi
+echo "$BUILD_MSG"
 cd "$(dirname "$0")/scripts"
